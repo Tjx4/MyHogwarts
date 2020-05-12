3214d49c8c6864672ab44cc2c1c2a8c9aad5c45f
Tshepo <rocboyt@gmail.com>
2020-05-12 21:17:57 +0200

    Added constants

---
+++ app/src/main/java/net/sure/myhogwarts/constants/Constants.kt
@@ -0,0 +1,16 @@
+package net.sure.myhogwarts.constants
+
+
+const val ACTIVITY_TRANSITION = "activity_transition"
+const val PAYLOAD_KEY = "payload"
+const val HOUSE = "house_key"
+const val CHARACTER = "character_key"
+const val SPELL = "spell_key"
+
+const val HOST = "https://www.potterapi.com/"
+const val GET_HOUSES = "v1/houses"
+const val GET_HOUSE = "v1/houses/{houseId}"
+const val GET_STUDENTS = "v1/characters"
+const val GET_STUDENT = "v1/characters/{studentId}"
+const val GET_SPELLS = "v1/spells"
+const val GET_SPELL = "v1/spells/{spellId}"
